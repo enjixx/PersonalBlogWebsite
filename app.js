e73da51652e639a7d46f47297cdd3269bef91e4d
Enkhlen Khurelbaatar <enkhulek@gmail.com>
2020-08-09 17:33:31 -0700

    Connected blog posts to mongoDB

--- app.js
+++ app.js
@@ -3,26 +3,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require("lodash");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+//Establish connection to the db
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let posts = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//New Post Schema
+const postSchema = {
+  title: String, 
+  content: String
+};
 
+//New post model
+const Post = mongoose.model("post", postSchema);
 
 app.get("/", function(req, res){
-  
-  res.render("home", {posts: posts});
-  
+
+  Post.find(function(err, postItems){
+    
+     res.render("home", {posts: postItems});
+  });
 });
 
 app.get("/about", function(req, res){
@@ -35,38 +48,34 @@ app.get("/contact", function(req, res){
 
 app.get("/compose", function(req, res){
   res.render("compose");
-  
 });
 
-app.get("/posts/:postName", function(req, res){
-  const routeName = _.lowerCase(req.params.postName);
-  
-  posts.forEach(function(post){
-    const postTitle = _.lowerCase(post.title);
-    if(routeName===postTitle){
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-    
-    console.log(routeName);
-    console.log(postTitle);
-    
-});
-    
+app.get("/posts/:postID", function(req, res){
+ // const routeName = _.lowerCase(req.params.postName);
+  const requestedPostID = req.params.postID;
+
+  Post.findOne({_id:requestedPostID}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  }); 
 });
 
 app.post("/compose", function(req, res){
-
-  const newPost = {
-    title: req.body.newPostTitle,
-    content: req.body.newPostText
-  }
-
-  posts.push(newPost);
-  res.redirect("/");
+  const postTitle = req.body.newPostTitle;
+  const postContent = req.body.newPostText;
   
+  const newPost = new Post({
+    title: postTitle,
+    content: postContent
+  });
+
+  newPost.save(function(err){
+    if(!err){
+       res.redirect("/");
+    }
+  });
 });
 
 
